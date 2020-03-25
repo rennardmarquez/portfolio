62d928a6f40b709ac4dcf26cda6f2339c3977738
rennard <marquezrennard@gmail.com>
2020-03-25 23:22:52 +0800

    style sidebar and header

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Menu.scss";
 
+//components
+import { ReactComponent as Logo } from "../../assets/rm-logo.svg";
+
 const Menu = () => (
-  <div className="menu">
-    <div className="about">About</div>
-    <div className="projects">Projects</div>
-    <div className="Contact">Contact</div>
-    <div>+</div>
+  <div className="header">
+    <Logo className="logo" />
+
+    <div className="menu">
+      <div className="about">About</div>
+      <div className="projects">Projects</div>
+      <div className="Contact">Contact</div>
+      <div>+</div>
+    </div>
   </div>
 );
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Sidebar.scss";
 
+//components
+import { ReactComponent as GithubLogo } from "../../assets/github.svg";
+import { ReactComponent as LinkedInLogo } from "../../assets/linkedin.svg";
+import { ReactComponent as TwitterLogo } from "../../assets/twitter.svg";
+
 const Sidebar = () => (
   <div className="sidebar">
-    <div className="home-button"></div>
-    <div className="social-media"></div>
+    <div className="social-media">
+      <GithubLogo className="icon" />
+      <LinkedInLogo className="icon" />
+      <TwitterLogo className="icon" />
+    </div>
   </div>
 );
 
