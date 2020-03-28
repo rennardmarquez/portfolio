2801129750ea509720fe22a20de6b814750fdb51
rennard <marquezrennard@gmail.com>
2020-03-28 09:43:36 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.scss";
 //Components
 import Main from "./components/Main/Main";
 import Menu from "./components/Menu/Menu";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div className="App">
       <Menu />
       <Main />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => (
+  <div className="footer">
+    <span>© Rennard Marquez. 2020 · All rights reserved</span>
+  </div>
+);
+
+export default Footer;
